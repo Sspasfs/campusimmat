86857a113c2773d68340144115271e04158c5b95
Sspasfs <136854659+Sspasfs@users.noreply.github.com>
2024-04-11 16:35:05 +0200

    Modification du start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Vérification et installation du pilote MySQL
-mysql_driver_path="/usr/lib/php/20190902/mysql.so" # Chemin vers le pilote MySQL
-if [ ! -f "$mysql_driver_path" ]; then
-    echo "Le pilote MySQL n'est pas installé. Installation en cours..."
-    
-    # Téléchargement du pilote MySQL
-    wget -O /tmp/mysql.so "https://github.com/php/pecl-database-mysql/releases/download/mysql-2.0.4/mysql-2.0.4.tgz"
-    
-    # Extraction du pilote
-    tar -xzf /tmp/mysql-2.0.4.tgz -C /tmp/
-    
-    # Copie du pilote dans le répertoire approprié
-    sudo cp /tmp/mysql.so "$mysql_driver_path"
-    
-    echo "Installation du pilote MySQL terminée."
-else
-    echo "Le pilote MySQL est déjà installé."
-fi
+# Suppression de la base de données
+php artisan migrate:reset
 
 # Exécuter les migrations
 php artisan migrate
